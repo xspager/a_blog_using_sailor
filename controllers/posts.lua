2fdf57e272257eac64a819d31a1a01f52430f524
Daniel Lemos <xspager@gmail.com>
2016-02-23 13:10:17 +0000

    List .md files on posts index and display them if use the filename as action

--- controllers/posts.lua
+++ controllers/posts.lua
@@ -1,9 +1,16 @@
+local lfs = require("lfs")
+
 local posts = {}
 
 function posts.index(page)
-    page:render("md", {
-        md_file_path="views/posts/index.md"
-    })
+    local my_posts = {}
+    for file in lfs.dir("views/posts/") do
+        local name_without_extention = string.match(file, "(.*)%.md$")
+        if name_without_extention then
+            table.insert(my_posts, name_without_extention)
+        end
+    end
+    page:render("index", {posts=my_posts})
 end
 
 setmetatable(posts, {
@@ -16,6 +23,11 @@ setmetatable(posts, {
             local my_posts = {
                 freebsd = "views/posts/sailor_on_freebsd.md",
             }
+            setmetatable(my_posts, {
+                __index = function(t, key)
+                    return "views/posts/"..key..".md"
+                end
+            })
 
             page.title = "Teste"
 
